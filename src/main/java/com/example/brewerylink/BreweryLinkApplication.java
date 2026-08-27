63d1c786cdc8aaaed41c04fb7901b095b2120291
kunizaki-y <be.castaway.crossout@ezweb.ne.jp>
2026-08-28 00:58:54 +0900

    [ 20260828 - DB接続・Userエンティティ初期設定 - 実装中 ]

---
+++ src/main/java/com/example/brewerylink/BreweryLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.brewerylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/brewerylink/BreweryLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brewerylink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
